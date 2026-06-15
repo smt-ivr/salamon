b4c0c3305c8402f53bb7bf4cec5196914e5d8044
smt-ivr <s6754091@gmail.com>
2026-06-15 13:12:00 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 import htmlContent from './index.js';
 import cssContent from './style.js';
-import jsContent from './frontend.txt'; // זכור ששינינו ל-txt
+import jsContent from './frontend.txt'; // הקובץ הקיים
+import messagesJsContent from './messages-client.js'; // הקובץ החדש שהוספנו!
 
 export default {
     async fetch(request, env, ctx) {
@@ -22,6 +23,13 @@ export default {
         
         if (path === '/salamon/frontend.js') {
             return new Response(jsContent, {
+                headers: { 'Content-Type': 'application/javascript; charset=utf-8' }
+            });
+        }
+
+        // הנתיב החדש עבור קובץ הלוגיקה של ההודעות
+        if (path === '/salamon/messages.js') {
+            return new Response(messagesJsContent, {
                 headers: { 'Content-Type': 'application/javascript; charset=utf-8' }
             });
         }
